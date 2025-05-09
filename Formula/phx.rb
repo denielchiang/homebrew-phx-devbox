a965554df25c9ea517221b4e02a3976ddb72f435
deniel <deniel@room3327.net>
2025-05-09 15:23:39 +0800

    v0.1.2 release

--- Formula/phx.rb
+++ Formula/phx.rb
@@ -1,9 +1,9 @@
 class Phx < Formula
   desc "Phoenix Docker project helper"
   homepage "https://github.com/denielchiang/phx-devbox"
-  url "https://github.com/denielchiang/phx-devbox/releases/download/v0.1.1/phx-devbox.tar.gz"
-  sha256 "e0dfc2bc5df0c732b98bcec20eacac2940f5516c8030d7808461daa0fd656125"
-  version "0.1.1"
+  url "https://github.com/denielchiang/phx-devbox/releases/download/v0.1.2/phx-devbox.tar.gz"
+  sha256 "40b2dd9c895fa27343edd38bf61de59e91f6c4f0961fe429f62538f1b1caaebf"
+  version "0.1.2"
 
   def install
     bin.install "bin/phx"
