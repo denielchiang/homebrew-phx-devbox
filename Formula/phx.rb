881ea0d8123d59872c895ea65c2a6517772f55a6
deniel <deniel@room3327.net>
2025-05-09 14:42:18 +0800

    v0.1.1

--- Formula/phx.rb
+++ Formula/phx.rb
@@ -3,7 +3,7 @@ class Phx < Formula
   homepage "https://github.com/denielchiang/phx-devbox"
   url "https://github.com/denielchiang/phx-devbox/releases/download/v0.1.1/phx-devbox.tar.gz"
   sha256 "e0dfc2bc5df0c732b98bcec20eacac2940f5516c8030d7808461daa0fd656125"
-  version "0.1.0"
+  version "0.1.1"
 
   def install
     bin.install "bin/phx"
