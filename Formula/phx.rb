8e33c3001990f77cee30e4434dd4df012c8dbb68
deniel <deniel@room3327.net>
2025-05-12 11:10:48 +0800

    Update phx formula to v0.1.4 with dockerfile fix

--- Formula/phx.rb
+++ Formula/phx.rb
@@ -1,9 +1,9 @@
 class Phx < Formula
   desc "Phoenix Docker project helper"
   homepage "https://github.com/denielchiang/phx-devbox"
-  url "https://github.com/denielchiang/phx-devbox/releases/download/v0.1.3/phx-devbox.tar.gz"
-  sha256 "bfe678f5f8b4d3e5d36dc89c8df8f0d811ff6c9419a3f8929c96b2d19e65d009"
-  version "0.1.3"
+  url "https://github.com/denielchiang/phx-devbox/releases/download/v0.1.4/phx-devbox.tar.gz"
+  sha256 "a2863daae13e9a68223cdd9d38f5bd8c419e6635e2ad2dde33f9f23d70662a0c"
+  version "0.1.4"
 
   def install
     bin.install "bin/phx"
