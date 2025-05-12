eb9cf9e503081e9b0d2f047f675caf0e85912296
deniel <deniel@room3327.net>
2025-05-12 17:14:26 +0800

    Fixed docker execution path with friendly error message

--- Formula/phx.rb
+++ Formula/phx.rb
@@ -2,7 +2,7 @@ class Phx < Formula
   desc "Phoenix Docker project helper"
   homepage "https://github.com/denielchiang/phx-devbox"
   url "https://github.com/denielchiang/phx-devbox/releases/download/v0.1.5/phx-devbox.tar.gz"
-  sha256 "35bd6d7db75e199589a8a34d0080ae67da632e354a11d3a800fb93f54daafe5d"
+  sha256 "4d83af35f59c940c288c7742ea36352da50fc3e3830245d7e1df3d35eb1f476a"
   version "0.1.5"
 
   def install
