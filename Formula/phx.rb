f6939b65478fccb3f14da46bdea24a7f040806c2
deniel <deniel@room3327.net>
2025-05-12 13:37:51 +0800

    Enhancement: Add support for custom Elixir/Erlang versions, version checking, and flexible parameter order

--- Formula/phx.rb
+++ Formula/phx.rb
@@ -2,7 +2,7 @@ class Phx < Formula
   desc "Phoenix Docker project helper"
   homepage "https://github.com/denielchiang/phx-devbox"
   url "https://github.com/denielchiang/phx-devbox/releases/download/v0.1.5/phx-devbox.tar.gz"
-  sha256 "21ef9131b745e6c978d6bf061fa6b00a0d55cb1c978a1dad960d2b507cee45a6"
+  sha256 "d13ea98675fbef1d155622ec228d6c1f52760559b450300e377611ac990d89e4"
   version "0.1.5"
 
   def install
