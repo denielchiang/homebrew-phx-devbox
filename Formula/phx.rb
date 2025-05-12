1d06b36e9d822c992c31df789ab9321fe0207a0c
deniel <deniel@room3327.net>
2025-05-12 10:59:21 +0800

    Update phx formula with new SHA256 for v0.1.3

--- Formula/phx.rb
+++ Formula/phx.rb
@@ -2,7 +2,7 @@ class Phx < Formula
   desc "Phoenix Docker project helper"
   homepage "https://github.com/denielchiang/phx-devbox"
   url "https://github.com/denielchiang/phx-devbox/releases/download/v0.1.3/phx-devbox.tar.gz"
-  sha256 "edf182e003581aa4eedb3fa0bcf15b597c1feaa5756b675ce4f6ee4a7a3bbe08"
+  sha256 "bfe678f5f8b4d3e5d36dc89c8df8f0d811ff6c9419a3f8929c96b2d19e65d009"
   version "0.1.3"
 
   def install
