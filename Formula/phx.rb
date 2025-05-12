0be3982ecb7e3718824eade1eb447a7b11776e60
deniel <deniel@room3327.net>
2025-05-12 14:25:28 +0800

    Update phx formula to version 0.1.5

--- Formula/phx.rb
+++ Formula/phx.rb
@@ -2,7 +2,7 @@ class Phx < Formula
   desc "Phoenix Docker project helper"
   homepage "https://github.com/denielchiang/phx-devbox"
   url "https://github.com/denielchiang/phx-devbox/releases/download/v0.1.5/phx-devbox.tar.gz"
-  sha256 "d13ea98675fbef1d155622ec228d6c1f52760559b450300e377611ac990d89e4"
+  sha256 "38b5cc03b7172c37dc7f34e3669359e8cdd1c8f4ade7a66552789872c5120290"
   version "0.1.5"
 
   def install
