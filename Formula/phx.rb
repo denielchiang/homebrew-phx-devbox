0b157fb3a720edb42fc23a1cfcc819cda304a83a
deniel <deniel@room3327.net>
2025-05-12 14:54:05 +0800

    Fixed docker execution path

--- Formula/phx.rb
+++ Formula/phx.rb
@@ -2,7 +2,7 @@ class Phx < Formula
   desc "Phoenix Docker project helper"
   homepage "https://github.com/denielchiang/phx-devbox"
   url "https://github.com/denielchiang/phx-devbox/releases/download/v0.1.5/phx-devbox.tar.gz"
-  sha256 "38b5cc03b7172c37dc7f34e3669359e8cdd1c8f4ade7a66552789872c5120290"
+  sha256 "891d6850704a8c60c3ecad0056fd7b20bbccc8b8a9401ef8ce852a5dbf85a7b3"
   version "0.1.5"
 
   def install
